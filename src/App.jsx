3a18d2fc9d1bacca84eddd9bb03244ba14a7836c
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2025-05-21 02:42:03 +1000

    feat: Render  MovieCard component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import MovieCard from './components/MovieCard'
 import './App.css'
 
 function App() {
   return (
     <>
+      <MovieCard movie={{title: "John's Movie", release_date: "2025"}}/>
+      <MovieCard movie={{title: "Mark's Movie", release_date: "2021"}}/>
     </>
   )
 }
